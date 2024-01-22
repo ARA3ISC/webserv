a95412de66bcc008e6e6575e58955751dd2c8125
Radouan Larabi <larabi.radouan@gmail.com>
2024-01-22 12:56:15 +0100

    add forbidden response in case of dir listing is off

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ RM = rm -f
 SRC = parsingConfigFile/webserv.cpp main.cpp parsingConfigFile/parsingConfigFile.cpp parsingConfigFile/server.cpp \
 parsingConfigFile/location.cpp utils/utils.cpp parsingConfigFile/error.cpp  \
 dataCenter/client.cpp utils/utils2.cpp utils/utils3.cpp \
-dataCenter/dataCenter.cpp dataCenter/parsingRequests.cpp methods/get.cpp
+dataCenter/dataCenter.cpp dataCenter/parsingRequests.cpp methods/get.cpp methods/Errors/ErrorDefaults.cpp
 OBJ_DIR = obj
 OBJ = $(addprefix $(OBJ_DIR)/, $(SRC:%.cpp=%.o))
 OBJ_SUBDIRS = $(sort $(dir $(OBJ)))
--- methods/get.cpp
+++ methods/get.cpp
@@ -1,53 +1,13 @@
 #include "../inc/dataCenter.hpp"
+#include "../inc/get.hpp"
 
 void cgi(std::string path){
-    
+    (void)path;
 }
 
 bool pathExists(const std::string& path) {
     struct stat buffer;
     return (stat(path.c_str(), &buffer) == 0);
-}
-
-std::string getDefaultError404(){
-    std::string err;
-    std::string line;
-    std::string path = "Errors/404.html";
-    std::ifstream input(path.c_str());
-
-    if (input.is_open()){
-        while (std::getline(input, line)) {
-            err += line;
-        }
-    }
-    return err;
-}
-
-std::string getContentFile(std::string path){
-    std::string fileContent = "";
-    std::string line;
-    std::ifstream input(path.c_str());
-
-    if (input.is_open()){
-        while (std::getline(input, line)) {
-            fileContent += line;
-        }
-    }
-    return fileContent;
-}
-
-void sendResponse(int clientSocket, int statusCode, std::string statusMessage, std::string content, std::string contentType) {
-    // Construct the HTTP response
-    std::ostringstream httpResponse;
-    httpResponse << "HTTP/1.1 " << statusCode << " " << statusMessage << "\r\n";
-    httpResponse << "Content-Type: " << contentType << "\r\n";
-    httpResponse << "Content-Length: " << content.length() << "\r\n";
-    httpResponse << "\r\n";
-    httpResponse << content;
-
-    // std::cout << "httpResponse : " << httpResponse.str() << std::endl;
-    // Send the response back to the client
-    write(clientSocket, httpResponse.str().c_str(), httpResponse.str().length());
 }
 
 int getLocationRequested(std::vector<location> loc, std::string path){
@@ -63,17 +23,6 @@ int getLocationRequested(std::vector<location> loc, std::string path){
             return i;
     }
     return -1;
-}
-
-void Error404(std::string fileName, int fd){
-
-
-    //content of the file provided in config file
-    std::string content = getContentFile(fileName);
-    // in case error file not provivde in config file we get our default file in Errors folder
-    if(content == "")
-        content = getDefaultError404();
-    sendResponse(fd, 404, "Not Found", content, "text/html; charset=utf-8");
 }
 
 void splitPath(const std::string& fullPath, std::string& directory, std::string& file) {
@@ -96,6 +45,7 @@ void splitPath(const std::string& fullPath, std::string& directory, std::string&
         file = "";
     }
 }
+
 void listDirectory(std::string path, std::string directory, int fd){
     
     DIR* dir;
@@ -142,6 +92,17 @@ void dataCenter::get(client clnt, int fd){
     else
     {
         std::cout << "directory\n";
+        if (srv.getLocations()[j].isAutoIndex()){
+            // sendResponse(fd, 200, "OK", getContentFile("Errors/403.html"), "text/html");
+
+        }
+        if (!srv.getLocations()[j].get_dir_listing())
+        {
+            sendResponse(fd, 403, "Forbidden", getContentFile("Errors/403.html"), "text/html");
+            return ;
+        }
+            // Error404(srv.get_error_pages()[404], fd);
+
         if (srv.getLocations()[j].isAutoIndex())
         {
             // auto index
