7c384d30f50f483de5c6ecb5b1e2a483dc17a12c
Radouan Larabi <larabi.radouan@gmail.com>
2024-01-24 18:57:30 +0100

    handling the requests of root && location inside location

--- methods/get.cpp
+++ methods/get.cpp
@@ -9,7 +9,17 @@ bool pathExists(const std::string& path) {
     struct stat buffer;
     return (stat(path.c_str(), &buffer) == 0);
 }
+bool isDirectory(const char* path) {
+    struct stat fileStat;
+    
+    if (stat(path, &fileStat) != 0) {
+        // Error handling: unable to retrieve file status
+        std::cerr << "Error getting file status for " << path << std::endl;
+        return false;
+    }
 
+    return S_ISDIR(fileStat.st_mode);
+}
 std::string cleanPath(std::string path) {
     std::string result;
 
@@ -29,7 +39,9 @@ std::string cleanPath(std::string path) {
     if (!result.empty() && result[0] != '/') {
         result = '/' + result;
     }
-
+    // add s single slash if it's empty
+    if (result.empty())
+        result = "/";
     return result;
 }
 
@@ -42,10 +54,6 @@ std::string getCleanPath(std::string path){
 
 int getLocationRequested(std::vector<location> loc, std::string path){
 
-    // here error when path is "/"
-//    path = cleanPath(path);
-//    std::cout << path << "---\n";
-
     for (size_t i = 0; i < loc.size(); i++)
         if (loc[i].getPath() == path)
             return i;
@@ -53,27 +61,32 @@ int getLocationRequested(std::vector<location> loc, std::string path){
     return -1;
 }
 
-void splitPath(const std::string& fullPath, std::string& directory, std::string& file) {
+void splitPath(std::string fullPath, std::string& directory, std::string& file) {
+
+    fullPath = cleanPath(fullPath);
     
-    // Find the last '/'
-    std::size_t lastSeparator = fullPath.find_last_of("/");
-    
-    if (lastSeparator == 0)
-    {
-        fullPath.substr(0);
-        lastSeparator = fullPath.find_last_not_of("/");
+    std::size_t last = fullPath.find_last_of("/");
+
+    if (last != std::string::npos){
+        
+        if(fullPath.substr(last + 1).find(".") != std::string::npos){
+            directory = fullPath.substr(0, last);
+            if (directory.empty())
+                directory = "/";
+            file = fullPath.substr(last + 1);
+        }
+        else
+        {
+            directory = fullPath;
+            file = "";
+        }
     }
-
-    if (lastSeparator != std::string::npos) {
-        // Extract the directory and file parts
-        directory = fullPath.substr(0, lastSeparator + 1);
-
-        file = fullPath.substr(lastSeparator + 1);
-    } else {
-        // No file part, the entire path is the directory
+    else
+    {
         directory = fullPath;
         file = "";
     }
+    
     // removing ? form the uri
     directory = getCleanPath(directory);
     file = getCleanPath(file);
@@ -84,17 +97,21 @@ void listDirectory(std::string path, std::string directory, int fd){
     DIR* dir;
     struct dirent* ent;
     std::string response = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Directory Listing</title></head><body><h1>Directory Listing</h1><ul>";
-    if ((dir = opendir(directory.c_str())) != NULL) {
+    if ((dir = opendir(path.c_str())) != NULL) {
         while ((ent = readdir(dir)) != NULL) {
             std::string tmp = ent->d_name;
             if (tmp == ".." || tmp == ".")
                 continue;
-            response += "<li><a href=\""  + path + "/" + tmp + "\" >" + tmp + "</a></li>";
+            if (directory == "/")
+                response += "<li><a href=\""  + directory + tmp + "\" >" + tmp + "</a></li>";
+            else
+                response += "<li><a href=\""  + directory + "/" + tmp + "\" >" + tmp + "</a></li>";
         }
         closedir(dir);
     }
     response += "</ul></body></html>";
     sendResponse(fd, 200, "OK", response, "text/html");
+    throw 0;
 }
 
 bool getContentIndexedFiles(std::string path, std::vector<std::string> index,std::string &content){
@@ -127,7 +144,6 @@ bool getContentIndexedFiles(std::string path, std::vector<std::string> index,std
         input.close();
     }
     
-    // std::cout << "*******directory of file to open " << path << "/" << index[0] << std::endl;
     return false;
 }
 
@@ -140,21 +156,19 @@ bool isMethodAllowed(std::vector<std::string> methods, std::string method){
 
 void dataCenter::get(client clnt, int fd){
     std::string directory, file;
-
     
-    server srv = getWebserv().getServers()[clnt.servIndx()];
-    
+    server srv = getWebserv().getServers()[clnt.servIndx()];    
 
     //split the directory and file fron the client request
     splitPath(clnt.getStartLine().path, directory, file); 
-
+    
     //get the index of the location 
     int j = getLocationRequested(srv.getLocations(), directory);
+    
     if (j == -1) {
         std::cout << "****\n";
         throw returnError(srv, fd, 404);
     }
-
 
     //is method allowed in config file
     if(isMethodAllowed(srv.getLocations()[j].getMethods(), "GET"))
@@ -164,6 +178,7 @@ void dataCenter::get(client clnt, int fd){
     std::string path = getCleanPath(srv.getLocations()[j].getRoot() + clnt.getStartLine().path);
     
     //cheking existing of the file on the server
+    
     if (!pathExists(path))
         throw returnError(srv, fd, 404);
 
@@ -191,8 +206,8 @@ void dataCenter::get(client clnt, int fd){
         if (!srv.getLocations()[j].get_dir_listing())
             throw returnError(srv, fd, 403);
         else // if auto index is false or none of the indexed file exist but dir_listing is true
-            listDirectory(clnt.getStartLine().path, path, fd);
+            listDirectory(srv.getLocations()[j].getRoot() + directory , directory, fd);
     }
-    std::cout << clnt.getFullRequest() << std::endl;
+    // std::cout << clnt.getFullRequest() << std::endl;
     // std::cout << clnt.servIndx() << "--" << path << " | " << clnt.getStartLine().path << std::endl;
 }
