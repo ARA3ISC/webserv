454ac6c7de5232154a07e6e2802b2763020bda4c
ara3isc <m.aneddame01@gmail.com>
2024-02-13 18:06:46 +0100

    change files in myWebsite

--- myWebsite/POST/display_contact.php
+++ myWebsite/POST/display_contact.php
@@ -9,25 +9,18 @@
 <body>
     <h1>Contact Information</h1>
     <?php
-    // Check if form data was submitted
 
-    if ($_SERVER["REQUEST_METHOD"] == "GET") {
-        // Retrieve and display submitted data
-        $name = $_GET["name"];
-        $time = $_GET["time"];
-        
-        // Display the submitted data
-        echo "<p><strong>Name:</strong> $name</p>";
-        echo "<p><strong>time:</strong> $time</p>";
-    } else {
-        // If no form data was submitted, display an error message
-        echo "<p>No contact information available.</p>";
-    }
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Retrieve and display submitted data
-        $name = $_POST["name"];
-        $time = $_POST["time"];
-        
+        if (isset($_POST["name"]))
+            $name = $_POST["name"];
+        else
+            $name = "";
+        if (isset($_POST["time"]))
+            $time = $_POST["time"];
+        else
+            $time = "";
+
         // Display the submitted data
         echo "<p><strong>Name:</strong> $name</p>";
         echo "<p><strong>time:</strong> $time</p>";
@@ -36,9 +29,7 @@
         echo "<p>No contact information available.</p>";
     }
     ?>
-        ?>
-    <a href="index.html">Home</a>
-    <a href="about.html">About</a>
     <a href="contact.html">Contact</a>
+    <a href="uploadFile.html">Upload File Chunekd</a>
 </body>
 </html>
